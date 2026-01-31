6b8f782c7564b684879891ce091cf357c8982f86
travellerTechnolegy <94226794+travellerTechnolegy@users.noreply.github.com>
2026-01-31 09:50:46 +0200

    Add files via upload
    The Dir class has been added and the Path and File classes have been changed.

--- path_all.py
+++ path_all.py
@@ -6,68 +6,30 @@ class Path:
         self.CWD = os.getcwd()  # текущая рабочая директория
         self.sep = os.sep
         if path:
-            self.current = os.fspath(path)
+            self.__current = os.fspath(path)
         else:
-            self.current = os.path.dirname(__file__)  # путь к родительскому каталогу, где расположен файл с классом Path
+            self.__current = os.path.dirname(__file__)  # путь к родительскому каталогу, где расположен файл с классом Path
+
+    def _get_current(self):
+        return self.__current
+    
+    def _set_current(self, path):
+        self.__current = os.fspath(path)
 
     def parent(self):
-        self.current = os.path.dirname(self.current)  # родительский путь на директорию выше
-        return self
+        self.__current = os.path.dirname(self)  # Так как есть __fspath__
     
-    def add_child(self, *dirname):
-        self.current = os.path.join(self.current, *dirname)
-        return self
-
-    def get_children(self, *, just_names=True):
-        if just_names:
-            return os.listdir(self.current)        
-        children = []
-        for child in os.scandir(self.current):
-            if child.is_dir():
-                obj = Path(child.path)
-                children.append(obj)
-        return children
-
     def exists(self):
-        return os.path.exists(self.current) 
+        return os.path.exists(self)  # Так как есть __fspath__ 
     
     def is_link(self):
-        return os.path.islink(self.current) 
+        return os.path.islink(self)  # Так как есть __fspath__
 
     def depth(self):
-        return len(self.current.split(self.sep)) - 1
+        return len(self.__current.split(self.sep)) - 1
     
     def get_path(self):
-        return self.current
-    
-    def create(self):
-        if not self.exists():
-            os.makedirs(self.current)
-        return self
-
-    def __add__(self, obj):  # позволяет добавлять новые каталоги
-        if isinstance(obj, str):
-            return Path(os.path.join(self.current, obj))
-        elif isinstance(obj, Path):
-            return Path(os.path.join(self.current, obj.current))
-        return NotImplemented
-    
-    def __radd__(self, obj):
-        if not isinstance(obj, str):
-            return NotImplemented
-        return Path(os.path.join(self.current, obj))  # Альтернативное поведение, когда у первого операнда нет соответствующего метода
-        
-    def __truediv__(self, obj):
-        if isinstance(obj, str):
-            return Path(os.path.join(self.current, obj))
-        elif isinstance(obj, Path):
-            return Path(os.path.join(self.current, obj.current))
-        return NotImplemented
-    
-    def __rtruediv__(self, obj):
-        if not isinstance(obj, str):
-            return NotImplemented
-        return Path(os.path.join(self.current, obj))
+        return self.__current   
 
     def __len__(self):
         """Returns path depth."""
@@ -79,75 +41,93 @@ class Path:
 
     def __eq__(self, obj):
         if isinstance(obj, str):
-            return self.current == obj
+            return self.__current == obj
         elif isinstance(obj, Path):
-            return self.current == obj.current
+            return self.__current == obj.__current
         return NotImplemented
-    
-    def __ne__(self, obj):
-        return self.current != obj.current
-    
-    # def __ne__(self, obj):  # Так делать нельзя!
-    #     return NotImplemented
     
     def __contains__(self, obj):
         if isinstance(obj, str):
-            return obj in self.current
+            return obj in self.__current
         elif isinstance(obj, Path):
-            return obj.current in self.current
+            return obj.__current in self.__current
         return NotImplemented   
 
     def __fspath__(self):
         return str(self)  # Служебный метод, делает наш объект (path) "похожим" на путь для использования в методе os.path.join.
         
     def __repr__(self):
-        return f"Path('{self.current}')"
+        return f"{self.__class__.__name__}('{self.__current}')"
 
     def __str__(self):
-        return self.current
+        return self.__current
     
+
+class Dir(Path):
+    # Класс Dir использует полностью __init__ класса Path
+    def add_child(self, *dirname):
+        self._set_current(os.path.join(self._get_current(), *dirname))
+        return self
+    
+    def get_children(self, *, just_names=True):
+        if just_names:
+            return os.listdir(self)        
+        children = []
+        for child in os.scandir(self):
+            if child.is_dir():
+                obj = Path(child.path)
+                children.append(obj)
+        return children
+    
+    def create(self):
+        os.makedirs(self)
+        return self
+    
+    def _join(self, obj):
+        return Dir(os.path.join(self, obj))
+    
+    def __add__(self, obj):  # позволяет добавлять новые каталоги
+        if isinstance(obj, str):
+            return self._join(obj)
+        elif isinstance(obj, Path):
+            return self._join(obj)
+        return NotImplemented
+    
+    def __radd__(self, obj):
+        if not isinstance(obj, str):
+            return NotImplemented
+        return self._join(obj)  # Альтернативное поведение, когда у первого операнда нет соответствующего метода
+        
+    def __truediv__(self, obj):
+        if isinstance(obj, str):
+            return self._join(obj)
+        elif isinstance(obj, Path):
+            return self._join(obj)
+        return NotImplemented
+    
+    def __rtruediv__(self, obj):
+        if not isinstance(obj, str):
+            return NotImplemented
+        return self._join(obj)
+
 
 class File(Path):
     def __init__(self, path):
         # Path.__init__(self, path) Для явного наследования __init__ (видно из какого класса)
-        super().__init__(path)  # ищет __init__ родительского класса и его запускает
-        self.current = os.fspath(path)  # переопределяем получение этого атрибута для класса File
+        super().__init__()  # ищет __init__ родительского класса и его запускает: исполняется код Path.__init__
+        self._set_current(os.fspath(path))  # переопределяем получение этого атрибута для класса File
 
     def depth(self):
-        return super().depth() - 1
-    
-    def create(self):
-        return None
-
-    def add_child(self, *dirname):
-        return None
-    
-    def get_children(self, *, just_names=True):
-        return None
+        return super().depth() - 1  # Исполняется Path.depth    
 
     def file_name(self):
-        return self.current.rsplit(self.sep)[-1]
+        return self._get_current().rsplit(self.sep)[-1]
     
-    def __add__(self, obj):
-        return NotImplemented
+    def parent(self):
+        return Dir(self).parent()  # Создаем экземпляр Dir как есть, и потом к нему применяем ЕГО метод parent
     
-    def __radd__(self, obj):
-        return NotImplemented
+        
     
-    def __truediv__(self, obj):
-        return NotImplemented
+
+
     
-    def __rtruediv__(self, obj):
-        return NotImplemented
-    
-    def __repr__(self):
-        return f"File('{self.current}')"
-    
-   
-
-
-
-
-
-
-
